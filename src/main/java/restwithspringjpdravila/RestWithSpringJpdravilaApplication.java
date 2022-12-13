e55483c58bf0fb0b13b78a1e255d34400bee22d4
João Pedro de Ávila <jpdravila@gmail.com>
2022-12-13 19:34:22 -0400

    Initial commit

---
+++ src/main/java/restwithspringjpdravila/RestWithSpringJpdravilaApplication.java
@@ -0,0 +1,15 @@
+package restwithspringjpdravila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringJpdravilaApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringJpdravilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restwithspringjpdravila/RestWithSpringJpdravilaApplicationTests.java
@@ -0,0 +1,13 @@
+package restwithspringjpdravila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringJpdravilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
